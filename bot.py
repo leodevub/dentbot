f06afb31b006ef861a7b67ea9c853b84e1992d35
Leo <leonardoduarteabreu@gmail.com>
2026-04-23 15:08:49 -0300

    atualiza url da api

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Cal
 import httpx
 from datetime import datetime
 
-API_URL = "http://localhost:8000"
+API_URL = "dentbot-production.up.railway.app"
 CLINICA_ID = "b5e74e86-e9c5-4547-a0b4-e1dcd207280d"
 SENHA_ADMIN = "dentsec2026"
 
